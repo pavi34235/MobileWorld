6c34912a5bcfbba899ccd3380305df21fa490230
pavi34235 <113873564+pavi34235@users.noreply.github.com>
2022-09-19 10:15:08 +0530

    Add files via upload

---
+++ Mobile World/pom/mobileworld/Login.java
@@ -0,0 +1,33 @@
+package mobileworld;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+@Test
+	public  void setup() throws InterruptedException {
+	
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\pavithra.s\\OneDrive - Qualitest Group\\chromedriver_win32//chromedriver.exe");
+	    WebDriver driver=new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	    driver.manage().window().maximize();
+	    
+	    Pom login=new Pom(driver);
+		   login.goTo();
+		   Thread.sleep(1000);
+		   login.loginPage("pavithra","pa1211");
+	
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\pavithra.s\\OneDrive - Qualitest Group\\chromedriver_win32//chromedriver.exe");
+        WebDriver driver1=new ChromeDriver();
+        driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver1.manage().window().maximize();
+
+        Pom login1=new Pom(driver1);
+            login1.goTo();
+            Thread.sleep(1000);
+            login1.loginPage("pa","pa@11");
+}
+}
---
+++ Mobile World/pom/mobileworld/mobile.java
@@ -0,0 +1,36 @@
+package mobileworld;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class mobile {
+@Test
+	public  void setup() {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\pavithra.s\\OneDrive - Qualitest Group\\chromedriver_win32//chromedriver.exe");
+	    WebDriver driver=new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	    driver.manage().window().maximize();
+	    Pom home=new Pom(driver);
+	    home.goTo();
+	    home.registrationPage("pavithra","shan","abc@gmail.com","abc123","3/12/2000","0123456789","Hi everyone");
+	    
+	    
+	    System.setProperty("webdriver.chrome.driver","C:\\Users\\pavithra.s\\OneDrive - Qualitest Group\\chromedriver_win32//chromedriver.exe");
+	    WebDriver driver1=new ChromeDriver();
+	    driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	    driver1.manage().window().maximize();
+	    Pom home1=new Pom(driver1);
+	    home1.goTo();
+	    home1.registrationPage("pa","sh","abc@gml.com","abc!123","3/12/2000","0126789","Hi everyone");
+	
+	
+	
+
+}
+
+	}
+
+
